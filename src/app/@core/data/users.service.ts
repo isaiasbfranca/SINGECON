8258f12e2f6abcc34534e4b2d9216d71343e894a
isaias.franca <isaiasbatista96@gmail.com>
2018-09-30 18:56:08 -0300

    subindo arquivos.

---
+++ src/app/@core/data/users.service.ts
@@ -0,0 +1,38 @@
+
+import { of as observableOf,  Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+
+let counter = 0;
+
+@Injectable()
+export class UserService {
+
+  private users = {
+    nick: { name: 'Isaias Batista', picture: 'assets/images/nick.png', title: 'Residencial teste' },
+    eva: { name: 'Eva Moor', picture: 'assets/images/eva.png', title: 'Residencial teste' },
+    jack: { name: 'Jack Williams', picture: 'assets/images/jack.png', title: 'Residencial teste'},
+    lee: { name: 'Lee Wong', picture: 'assets/images/lee.png', title: 'Residencial teste' },
+    alan: { name: 'Alan Thompson', picture: 'assets/images/alan.png', title: 'Residencial teste' },
+    kate: { name: 'Kate Martinez', picture: 'assets/images/kate.png', title: 'Residencial teste' },
+  };
+
+  private userArray: any[];
+
+  constructor() {
+    // this.userArray = Object.values(this.users);
+  }
+
+  getUsers(): Observable<any> {
+    return observableOf(this.users);
+  }
+
+  getUserArray(): Observable<any[]> {
+    return observableOf(this.userArray);
+  }
+
+  getUser(): Observable<any> {
+    counter = (counter + 1) % this.userArray.length;
+    return observableOf(this.userArray[counter]);
+  }
+}
