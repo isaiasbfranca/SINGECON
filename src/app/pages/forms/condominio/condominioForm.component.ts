63716befbf545ab03c5439a214c06a0dec96ee10
Cristiano Lopes <cristino.lopes@squadra.com.br>
2018-10-17 11:36:04 -0300

    condominioForm.

---
+++ src/app/pages/forms/condominio/condominioForm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-administradora-form',
+  styleUrls: ['./administradoraForm.component.scss'],
+  templateUrl: './administradoraForm.component.html',
+})
+export class AdministradoraFormComponent {
+
+}
